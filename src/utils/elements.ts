90a7ad9e0f2b60e1529bd962b06db7b6d4e97d39
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-09-28 09:37:39 +0300

    fix: fix type name

--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -1,5 +1,5 @@
 type ProfileButtons = Record<string, HTMLButtonElement | null>
-type SelecorObject = Record<string, string>
+type SelectorObject = Record<string, string>
 
 export const profileButtons: ProfileButtons = {
   avatarButton: document.querySelector('.profile__button_type_avatar'),
@@ -7,11 +7,17 @@ export const profileButtons: ProfileButtons = {
   addPlaceButton: document.querySelector('.profile__button_type_place')
 }
 
-export const popupConfig: SelecorObject = {
+export const popupConfig: SelectorObject = {
   avatarPopupSelector: '.popup_avatar',
   profileEditPopupSelector: '.popup_profile',
   addPlacePopupSelector: '.popup_place',
   imagePopupSelector: '.popup_image',
   deletePopupSelector: '.popup_delete',
   openedPopupClass: 'popup_opened'
+}
+
+export const profileSelectors: SelectorObject = {
+  avatar: '.profile__avatar',
+  name: '.profile__name',
+  about: 'profile__caption'
 }
